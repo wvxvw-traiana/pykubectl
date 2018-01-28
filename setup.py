40be08167572a901b79f8730847397b047a5da3b
Oleg Sivokon <olegs@traiana.com>
2018-01-28 11:59:14 +0200

    Added build command to build Go sources.

--- setup.py
+++ setup.py
@@ -2,10 +2,61 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import shutil
+import subprocess
 
 from setuptools import setup, Extension
 from Cython.Distutils import build_ext
+from setuptools.command.build_py import build_py
+
+
+LIB_DIR = '/usr/local/lib'
+
+
+class BuildGo(build_py):
+    '''
+    Build Go bindings before building Python extension
+    '''
+
+    def run(self):
+        version = None
+        refusal = 'Will not compile Go bindings'
+        try:
+            version = subprocess.check_output(['go', 'version'])
+        except subprocess.CalledProcessError:
+            print('Didn\'t find Go compiler.')
+            print(refusal)
+            return
+        match = re.search(br'go(\d+)\.(\d+)\.(\d+)', version)
+        if match:
+            version = tuple(map(int, match.group(1, 2, 3)))
+        else:
+            print('Unrecognized version of Go compiler: {}'.format(version))
+            print(refusal)
+            return
+        req_version = 1, 9, 1
+        if version < req_version:
+            print('You need Go compiler version higher than: {}'.format(
+                req_version,
+            ))
+            print(refusal)
+            return
+        try:
+            result = subprocess.check_output([
+                'go',
+                'build',
+                '-v',
+                '-buildmode=c-shared',
+                '-o',
+                'pykubectl/lib/libgokubectl.so',
+                'github.com/wvxvw-traiana/pykubectl/main',
+            ])
+            print(result)
+        except subprocess.CalledProcessError as e:
+            print(e.stderr)
+            raise
+        build_py.run(self)
 
 
 def is_extension_file(name):
@@ -33,11 +84,15 @@ setup(
     author_email='olegs@traiana.com',
     url='TBD',
     license='PROPRIETARY',
-    cmdclass={"build_ext": build_ext},
+    cmdclass={
+        'build_ext': build_ext,
+        'build_py': BuildGo,
+    },
     scripts=['bin/pykubectl'],
     # See: http://blog.codekills.net/2011/07/15/\
     # lies,-more-lies-and-python-packaging-documentation-on--package_data-/
-    package_data={'pykubectl': ['lib/libgokubectl.so']},
+    # package_data={'pykubectl': ['lib/libgokubectl.so']},
+    data_files=[(LIB_DIR, ['pykubectl/lib/libgokubectl.so'])],
     ext_modules=[
         Extension(
             'pykubectl.wrapped',
@@ -48,8 +103,7 @@ setup(
             include_dirs=['pykubectl/lib'],
             library_dirs=['pykubectl/lib'],
             libraries=['gokubectl'],
-            extra_link_args=['-Wl,-rpath,/usr/local/lib'],
-            # -Wl,+s
+            extra_link_args=['-Wl,-rpath,{}'.format(LIB_DIR)],
             # runtime_library_dirs=[
             #     '/usr/local/lib/python3.6/site-packages/pykubectl-0.0.1-py3.6-linux-x86_64.egg/pykubectl/lib',
             #     './lib', './pykubectl/lib', '/usr/local/lib',
